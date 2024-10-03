882b7c642cf86a6c9aec688c282de36f7066502c
Bhavin Pun <bhpun@MF2CH9R9L0.ad.here.com>
2024-10-03 19:46:34 +0530

    Titlte: head title and image configuration Date: 3 october2024 Desc: header is done and the image cofniguration is done

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export', // Add this line for static export
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,16 @@
 import store from "@/redux/store";
 import "@/styles/globals.css";
+import Head from "next/head";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
   return (
     // <div data-theme="hds-web-product-light-theme" data-styles="hds" id="container">
     <Provider store={store}>
+      <Head>
+        <title>Tracking Coronavirus Covid-19</title>
+        <meta name="description" content="Default description for all pages" />
+      </Head>
       <Component {...pageProps} />
     </Provider>
     // </div>
