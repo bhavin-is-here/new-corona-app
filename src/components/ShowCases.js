a1e9b302b798da4e74ddc1a204b54a227ac34a0c
Bhavin Pun <bhpun@MF2CH9R9L0.ad.here.com>
2024-11-05 17:31:37 +0530

    Title: sidebar responsive fix for the blog Developer:Bhavin

--- src/components/ShowCases.js
+++ src/components/ShowCases.js
@@ -7,14 +7,22 @@ const ShowCases = ({worldCase}) => {
 
     return (
         <div className='show-cases'>
+            <div className='show-case-box'>
+
             <div className='confirm-cases' >
+                <div className='confirm-bg-box'>
                 <p className='confirm-case-title' >Confirmed cases</p>
+                </div>
                 <p className='confirm-case-number'> {selectedCountry.confirmCases ? formatNumberWithCommas(selectedCountry.confirmCases) : formatNumberWithCommas(worldCase?.total_cases)}</p>
             </div>
             <div className='confirm-cases'>
+                <div className='confirm-bg-box'>
                 <p className='confirm-case-title'>Confirmed deaths</p>
+                </div>
                 <p className='confirm-case-number'>{selectedCountry.confirmDeaths ? formatNumberWithCommas(selectedCountry.confirmDeaths) : formatNumberWithCommas(worldCase?.total_deaths)}</p>
             </div>
+            </div>
+
         </div>
     )
 }
